57af20f4c9d86f9c8e47e55010863af1036e1234
Deck Stream <tneswick2@verizon.net>
2022-06-11 09:48:23 -0700

    issue #3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/nav';
 import About from './components/about';
+import Gallery from './components/gallery';
+
+const [categories] = useState([
+  {
+    name: 'commercial',
+    description: 'Photos of grocery stores, food trucks, and other commercial projects',
+  },
+  { name: 'portraits', description: 'Portraits of people in my life' },
+  { name: 'food', description: 'Delicious delicacies' },
+  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+]);
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
 function App() {
   return (
     <div>
-      <Nav></Nav>
+      <Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      </Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
